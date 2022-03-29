6a674a19e2083d6aa41bf24c1bed306ad9c31ab0
Ваше Имя <you@example.com>
2022-03-30 01:14:22 +0300

    at least trainig starts...

--- test/train_model_test.py
+++ test/train_model_test.py
@@ -2,7 +2,7 @@ import pytorch_lightning as pl
 from running.wraped_model import VarMisuseLayer
 import yaml
 from data_processing import vocabulary, data_loader
-
+from torch.utils.data import DataLoader
 
 data_path = '../data'
 config_path = '../config.yml'
@@ -14,5 +14,5 @@ vocab = vocabulary.Vocabulary(vocab_path=vocabulary_path)
 data = data_loader.GraphDataset(data_path, vocab, config, mode, debug=False)
 model = VarMisuseLayer(config['model'], config['training'], data.vocabulary.vocab_dim)
 
-trainer = pl.Trainer(gpus=1)
-trainer.fit(model=model, train_dataloaders=data)
+trainer = pl.Trainer()
+trainer.fit(model=model, train_dataloaders=DataLoader(data, batch_size=32))
