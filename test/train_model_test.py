a72ee4c375b2245057c82721bbeb4ca1f2318aef
Ваше Имя <you@example.com>
2022-03-30 03:20:28 +0300

    lightning data_loader added

--- test/train_model_test.py
+++ test/train_model_test.py
@@ -2,17 +2,17 @@ import pytorch_lightning as pl
 from running.wraped_model import VarMisuseLayer
 import yaml
 from data_processing import vocabulary, data_loader
-from torch.utils.data import DataLoader
 
 data_path = '../data'
 config_path = '../config.yml'
 vocabulary_path = '../vocab.txt'
-mode = 'test'
 
 config = yaml.safe_load(open(config_path))
 vocab = vocabulary.Vocabulary(vocab_path=vocabulary_path)
-data = data_loader.GraphDataset(data_path, vocab, config, mode, debug=False)
+data = data_loader.MyDataLoader(data_path, vocab, config)
+data.prepare_data()
+data.setup('fit')
 model = VarMisuseLayer(config['model'], config['training'], data.vocabulary.vocab_dim)
 
 trainer = pl.Trainer()
-trainer.fit(model=model, train_dataloaders=DataLoader(data, batch_size=32))
+trainer.fit(model=model, train_dataloaders=data.train_dataloader())
