a03e50a810b9a2984bc349d49b0fe86dd461c46a
Худяков Юрий <catchitoo@mail.ru>
2022-05-27 04:17:17 +0300

    Fix padding mask type

--- models/utils.py
+++ models/utils.py
@@ -5,11 +5,6 @@ import torch
 
 def generate_square_subsequent_mask(sz, device):
     mask = (torch.triu(torch.ones((sz, sz), device=device)) == 1).transpose(0, 1)
-    mask = (
-        mask.float()
-        .masked_fill(mask == 0, float("-inf"))
-        .masked_fill(mask == 1, float(0.0))
-    )
     return mask
 
 
