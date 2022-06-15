f8e4f5ad6e8328691c16f577ac76500a457922bf
Худяков Юрий <catchitoo@mail.ru>
2022-06-15 17:46:17 +0300

    Fix pad id in iterable dataset

--- data_processing/graph_var_miner/graph_var_miner_dataset_iterable.py
+++ data_processing/graph_var_miner/graph_var_miner_dataset_iterable.py
@@ -158,7 +158,7 @@ class GraphVarMinerDatasetIterable(Dataset, IterableDataset):
     def _process_tokens(self, tokens: list) -> torch.Tensor:
         out_tensor = np.zeros(
             (len(tokens), self._max_token_len), dtype=int
-        )  # SUPER IMPORTANT TODO: fix with pad_id
+        ) * self._vocabulary.pad_id()
         for i, token in enumerate(tokens):
             if self._preprocessed:
                 enc = token
